8c7ab3556c3e29bb0d1319b9902691b07a4f18d1
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-14 17:07:27 +0530

    using consumer method for getting data from context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import LanguageContext from '../contexts/LanguageContext'
 export default class Button extends Component {
-    // we are adding a property to class
-    static contextType = LanguageContext;
+    // we are adding a property to class 
+    // static contextType = LanguageContext;
+
+    // when using consumer we do not need contextType and this.context
 
     render() {
         // console.log(this.context);
-        const text = this.context === 'english' ? 'submit' : 'voorleggen'
+        // const text = this.context === 'english' ? 'submit' : 'voorleggen'
 
         return (
             <div>
-                <button className='ui button primary'>{text}</button>
+                <button className='ui button primary'>
+                    <LanguageContext.Consumer>
+                        {(value) => value === 'english' ? 'submit' : 'voorleggen'}
+                    </LanguageContext.Consumer>
+                </button>
             </div>
         )
     }
